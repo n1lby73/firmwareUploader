8c29e8143294d6bf160e76e55eb474b961eaf95f
n1lby73 <n1lby73@gmail.com>
2025-04-15 11:52:47 +0100

    explicity compile before uploading

--- uploader/avrUploader.py
+++ uploader/avrUploader.py
@@ -15,6 +15,34 @@ def uploadFirmwareAvr(firmwareFile, port, boardVariant):
 
     """Upload firmware to an AVR microcontroller."""
 
+    # Compile code
+
+    try:
+        
+        avrd_compile = [
+
+            "arduino-cli",  # Path to arduino-cli (make sure it's in your PATH)
+            "compile",  # Command to upload the firmware
+            "--fqbn", f"arduino:avr:{boardVariant}",  # Specify the board (e.g., arduino:avr:uno)
+            "--port", port,  # Specify the serial port (e.g., /dev/ttyACM0 or COM3)
+            firmwareFile  # Path to the firmware file (blink.ino)
+
+        ]
+        
+        # Upload the firmware
+
+        messagebox.showinfo("Firmware upload", message)
+        subprocess.run(avrd_compile, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        messagebox.showinfo("Firmware upload", "Successfully uploaded")
+
+    except subprocess.CalledProcessError as e:
+
+        error_message = e.stderr
+        
+        # Display the upload error message in a messagebox
+        messagebox.showerror("Upload Error", message=error_message)
+    
+    # Upload code
     try:
 
         avrd_upload = [
